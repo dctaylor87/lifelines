28d27a28d0175a19e5267cdf667bb4ed45b92025
David Taylor <dctaylor87@gmail.com>
2023-08-14 18:23:13 -0400

    Add 'const' on buffer argument of output/error funcs -- we never modify it.

--- src/ui/uiioi.h
+++ src/ui/uiioi.h
@@ -3,6 +3,8 @@
 
 /* This is for the *INTERNALS*.  For the API, please see hdrs/uiio.h instead */
 
+/* XXX we might want to rethink how to do i/o here! XXX */
+
 struct uiio
 {
   void *uiio_input_data;
@@ -11,11 +13,13 @@ struct uiio
 
   /* tentative argument lists and return values */
   int (*uiio_input_func)(void *data, char **buffer, int *length, char **err_msg);
-  int (*uiio_output_func)(void *data, char *buffer, char **err_msg);
-  int (*uiio_error_func)(void *data, char *buffer, char **err_msg);
+  int (*uiio_output_func)(void *data, const char *buffer, char **err_msg);
+  int (*uiio_error_func)(void *data, const char *buffer, char **err_msg);
 
   /* for errors, we use uiio_error_data,
      for status and info we use uiio_output_data */
   void (*uiio_outputv_func)(void *data,
 			    MSG_LEVEL level, STRING fmt, va_list args);
 };
+
+extern const char *ui_prompt;
